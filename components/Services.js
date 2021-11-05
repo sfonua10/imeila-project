970e2b42934b813e5d667ece629a256999ba85b2
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2021-11-04 19:43:20 -0600

    add services

--- components/Services.js
+++ components/Services.js
@@ -1,5 +1,49 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
+
+const features = [
+  {
+    name: 'Tech-Enabled Candidate Sourcing',
+    description:
+      '',
+    icon: GlobeAltIcon,
+  },
+  {
+    name: 'Full-Cycle Recruiting Support',
+    description:
+      '',
+    icon: ScaleIcon,
+  },
+  {
+    name: 'HR Strategy and Planning',
+    description:
+      '',
+    icon: LightningBoltIcon,
+  },
+]
+
 export default function Services() {
-    return(
-        <h1>YO</h1>
-    )
+  return (
+    <div className="py-12 bg-white">
+      <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl text-center mb-8">
+          What we offer:
+        </h2>
+        <h2 className="sr-only">What we offer</h2>
+        <dl className="flex items-center justify-evenly text-center" style={{textAlign: '-webkit-center'}}>
+          {features.map((feature) => (
+            <div key={feature.name}>
+              <dt>
+                <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <feature.icon className="h-6 w-6" aria-hidden="true" />
+                </div>
+                <p className="mt-5 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+              </dt>
+              <dd className="mt-2 text-base text-gray-500">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
+  )
 }
--- components/TrustedBrands.js
+++ components/TrustedBrands.js
@@ -3,7 +3,7 @@ const TrustedBrands = () => {
     <div className="bg-gray-100">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <p className="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">
-          Trusted by over 5 very average small businesses
+          Trusted by Top Brands
         </p>
         <div className="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
